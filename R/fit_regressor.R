f3880307d14db6a8792f699ecb53f29e578064a3
Reza Mirzazadeh <mrezamirzazadeh@gmail.com>
2022-01-20 21:05:21 -0800

    Added documention

--- R/fit_regressor.R
+++ R/fit_regressor.R
@@ -1,1 +1,14 @@
+#' Fit dummy regressor and linearregression models 
+#'
+#' @return A data frame
+#' @export
+#'
+#' @examples
+#' data <- read_csv("data/spotifyfeatures.csv")
+#' fit_regressor(data, target_col = 'popularity', categorical_features='genre')
+#' fit_regressor(data, target_col = 'popularity', numeric_feats = ['danceability', 'loudness'], categorical_feats=['genre'], cv=10)
+#' 
+#' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(3, 6, 12, 18, 24))
+#' fit_regressor(df, target_col = 'y', numeric_feats='x')
+#' fit_regressor(df, target_col = 'y')
 fit_regressor <- function(){}

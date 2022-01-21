1645e1b0e289939c49f3421fc2b1a594eb9c1bf8
Sanchit Singh <sanchit120496@gmail.com>
2022-01-21 12:19:57 -0800

    Adding the function roxygen file fixed typo

--- R/fit_classifier.R
+++ R/fit_classifier.R
@@ -8,6 +8,6 @@
 #' fit_classifier(data, target_col = 'income', numeric_feats = list('age', 'fnlwgt', 'hours.per.week', 'education.num', 'capital.gain', 'capital.loss'), categorical_feats=list('genre'), cv=10)
 #' 
 #' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(0, 1, 1, 0, 1))
-#' fit_classifier(df, target_col = 'y', numeric_feats=lsit('x'))
+#' fit_classifier(df, target_col = 'y', numeric_feats=list('x'))
 #' fit_classifier(df, target_col = 'y')
 fit_classifier <- function(train_df, target_col, numeric_feats= NULL, categorical_features= NULL, cv = 5){}

dad364ec0b47c5245aeee397c8552a9d37b26402
zzhzoe <zoe.zzh121@gmail.com>
2022-01-21 13:58:29 -0800

    clean_data

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,1 +1,13 @@
-clean_data <- function(){}
+#'Clean_data
+#'Load downloaded data, clean the dataset, remove the NA score. 
+#'Clean data(remove Nan rows, strip extra white spaces from column names, and data, convert all column names to lower case, etc)
+#'
+#' @param  data.frame Data set to clean
+#'
+#' @return data.frame A cleaned and simplified DataFrame of the relevant columns for summary and visualization
+#' @export
+#'
+#' @examples
+#' df <- clean_data(data)
+
+clean_data <- function(data.frame){}

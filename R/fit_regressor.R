14ca8833fb1d9b3b0df2154767059612fc9e9d3c
Reza Mirzazadeh <mrezamirzazadeh@gmail.com>
2022-01-20 21:22:34 -0800

    minor changes

--- R/fit_regressor.R
+++ R/fit_regressor.R
@@ -4,11 +4,10 @@
 #' @export
 #'
 #' @examples
-#' data <- read_csv("data/spotifyfeatures.csv")
 #' fit_regressor(data, target_col = 'popularity', categorical_features='genre')
 #' fit_regressor(data, target_col = 'popularity', numeric_feats = ['danceability', 'loudness'], categorical_feats=['genre'], cv=10)
 #' 
 #' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(3, 6, 12, 18, 24))
 #' fit_regressor(df, target_col = 'y', numeric_feats='x')
 #' fit_regressor(df, target_col = 'y')
-fit_regressor <- function(){}
+fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical_features= NULL, cv = 5){}

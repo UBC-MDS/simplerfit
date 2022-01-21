a8b20ee2327e8b8bf4ff197af4d1410f2fcd8f92
Sanchit Singh <sanchit120496@gmail.com>
2022-01-21 11:52:08 -0800

    Adding the function roxygen file with list changes

--- R/fit_classifier.R
+++ R/fit_classifier.R
@@ -4,10 +4,10 @@
 #' @export
 #'
 #' @examples
-#' fit_classifier(data, target_col = 'income', categorical_features='occupation')
-#' fit_classifier(data, target_col = 'income', numeric_feats = ['age', 'fnlwgt', 'hours.per.week', 'education.num', 'capital.gain', 'capital.loss'], categorical_feats=['genre'], cv=10)
+#' fit_classifier(data, target_col = 'income', categorical_features=list('occupation'))
+#' fit_classifier(data, target_col = 'income', numeric_feats = list('age', 'fnlwgt', 'hours.per.week', 'education.num', 'capital.gain', 'capital.loss'), categorical_feats=list('genre'), cv=10)
 #' 
 #' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(0, 1, 1, 0, 1))
-#' fit_classifier(df, target_col = 'y', numeric_feats='x')
+#' fit_classifier(df, target_col = 'y', numeric_feats=lsit('x'))
 #' fit_classifier(df, target_col = 'y')
 fit_classifier <- function(train_df, target_col, numeric_feats= NULL, categorical_features= NULL, cv = 5){}

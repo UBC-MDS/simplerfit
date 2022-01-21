ad1ba250a862b4d258b1cc0b767b86c7ae47e2a6
Navya Dahiya <navyad265@gmail.com>
2022-01-21 14:39:57 -0800

    Modified the description of roxygen and changes input param from input data type to a variable

--- R/clean_data.R
+++ R/clean_data.R
@@ -1,13 +1,11 @@
 #'Clean_data
-#'Load downloaded data, clean the dataset, remove the NA score. 
-#'Clean data(remove Nan rows, strip extra white spaces from column names, and data, convert all column names to lower case, etc)
-#'
+#'Load downloaded data, clean the dataset, strip extra white spaces from column names, convert column names to lower case, remove the NA rows and return clean data
 #' @param  data.frame Data set to clean
 #'
-#' @return data.frame A cleaned and simplified DataFrame of the relevant columns for summary and visualization
+#' @return data A cleaned and simplified DataFrame of the relevant columns for summary and visualization
 #' @export
 #'
 #' @examples
 #' df <- clean_data(data)
 
-clean_data <- function(data.frame){}
+clean_data <- function(data){}

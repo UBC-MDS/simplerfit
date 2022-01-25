2f85fc4be2bbf9004cae305c2f73fbe57c1df59c
nd265 <navyad265@gmail.com>
2022-01-24 22:50:40 -0800

    Add code for plot_corr and test files

--- R/plot_corr.R
+++ R/plot_corr.R
@@ -3,12 +3,22 @@
 
 #' @param data a dataframe
 #' @param corr type of correlation: 'spearman', 'kendall' or 'pearson'
+#' @param pair_cols : a character vector od columns for which you want correlation plot
 #' @return corr_plot, a ggplot chart object
 #' @export
 #'
 #' @examples
 #' data <- read_csv("../tests/Spotify_Features.csv")
-#' corr_plot(data, corr = 'pearson')
+#' corr_plot(data, corr = 'pearson', pair_cols=c('age', 'education.num'))
 #' corr_plot(data)
 
-plot_corr <- function(data, corr='spearman'){}
+plot_corr <- function(data, corr='pearson', pair_cols=NULL){
+  if(!is.null(pair_cols)){
+
+    data <- data |>
+      select(pair_cols)
+  }
+  corr_plot <- GGally::ggcorr(data,low = "steelblue", mid = "white", high = "darkred", label=TRUE,method = c("everything", corr) )
+  corr_plot
+
+}

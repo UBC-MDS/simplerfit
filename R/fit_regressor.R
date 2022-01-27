bdb2dac55fcc9fa56f727fa0f7c3061460453704
Reza Mirzazadeh <mrezamirzazadeh@gmail.com>
2022-01-27 12:28:52 -0800

    minor changes function is working

--- R/fit_regressor.R
+++ R/fit_regressor.R
@@ -1,31 +1,44 @@
-#' Fit dummy regressor and linearregression models 
+library(Matrix)
+library(mltools)
+library(readr)
+library(tidyverse)
+library(testthat)
+library(dplyr)
+library(data.table)
+library(gapminder)
+library(lattice)
+library(caret)
+
+#' Fit dummy regressor and linear regression models 
 #'
 #' @param train_df dataframe that will be used to train the model
 #' @param target_col The column that needs to be classified as a string
 #' @param numeric_feats The numeric columns as a vector character
 #' @param categorical_feats The categorical columns as a vector character
-#' @param cv The number of crossvalidation folds as an integer
+#' @param cv The number of cross validation folds as an integer
+#' @param st_seed The number of seedto setup
 #' 
 #' @return A data frame
 #' @export
 #'
 #' @examples
-#' fit_regressor(data, target_col = 'popularity', categorical_feats=c('genre'))
-#' fit_regressor(data, target_col = 'popularity', numeric_feats = c('danceability', 'loudness'), categorical_feats=c('genre'), cv=10)
+#' library(lattice)
+#' library(caret)
+#' library(readr)
+#' library(tidyverse)
+#' library(testthat)
+#' library(dplyr)
+#' library(mltools)
+#' library(data.table)
+#' library(gapminder)
+#' fit_regressor(data, target_col = 'gdpPercap', categorical_feats=c('continent'))
+#' fit_regressor(data, target_col="gdpPercap", numeric_feats=c("year", "lifeExp", "pop"), categorical_feats <- c("continent"), cv =5, seed =124)
 #' 
 #' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(3, 6, 12, 18, 24))
 #' fit_regressor(df, target_col = 'y', numeric_feats=c('x'))
 #' fit_regressor(df, target_col = 'y')
 
-library(caret)
-library(readr)
-library(tidyverse)
-library(testthat)
-library(dplyr)
-library(mltools)
-library(data.table)
-
-fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical_feats= NULL, cv = 5){
+fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical_feats= NULL, cv = 5, set_seed = 123){
   
   #test for input types : 
 
@@ -46,13 +59,13 @@ fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical
   })
   
   
-  #tests for imputs values
+  #tests for inputs values
   
   test_that("Train dataframe should be clean", {
     expect_equal(sum(is.na(train_df)), 0)
   })
   test_that("target_col should be a column from numeric columns", {
-    expect_true(all(target_col %in% colnames(select_if(train_df, is.numeric)))),
+    expect_true(all(target_col %in% colnames(select_if(train_df, is.numeric))))
     expect_equal(length(target_col),1)
   })
   test_that("numeric_feats should be from numeric columns", {
@@ -68,7 +81,7 @@ fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical
   X_train_numeric <- scale(train_df[, numeric_feats])
   
   # One hot coding of categorical columns
-  train_df[categorical_feats)] <- lapply(train_df[categorical_feats], factor)
+  train_df[categorical_feats] <- lapply(train_df[categorical_feats], factor)
   X_train_categorical <- one_hot(as.data.table(train_df[categorical_feats]))
   
   #y_train
@@ -77,19 +90,23 @@ fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical
   # Combining pre-processed data
   train_preprocessed <- cbind(X_train_numeric, X_train_categorical, y_train)
   
-  # Model
-  set.seed(123) 
+  #create target_col
+  train_preprocessed$target_col_model <- train_preprocessed |> select(target_col)
+  train_preprocessed <- train_preprocessed |> select(-target_col)
   
-  #Dummyregressor
-  model_null <- train(popularity ~., data = train_preprocessed, method = "null", 
+  # Model
+  set.seed(set_seed) 
+  
+  #Dummy regressor
+  model_null <- train(target_col_model ~., data = train_preprocessed, method = "null", 
                     trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
   
   #Linear model
-  model_lm <- train(popularity ~., data = train_preprocessed, method = "lm", 
+  model_lm <- train(target_col_model ~., data = train_preprocessed, method = "lm",
                     trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
   
   #Ridge
-  model_ridge <- train(popularity ~., data = train_preprocessed, method = "bridge", 
+  model_ridge <- train(target_col_model~., data = train_preprocessed, method = "bridge",
                     trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
   
   #Result dataframe
@@ -106,4 +123,7 @@ fit_regressor <- function(train_df, target_col, numeric_feats= NULL, categorical
   return(results)
   
 }
-print("hi")
+
+data = gapminder
+fit_regressor(data, target_col="gdpPercap", numeric_feats=c("year", "lifeExp", "pop"), categorical_feats <- c("continent"), cv =5)
+

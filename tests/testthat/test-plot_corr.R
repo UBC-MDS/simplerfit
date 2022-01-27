9aaec13f14e8232448a5336709615e11478acb97
nd265 <navyad265@gmail.com>
2022-01-26 16:31:04 -0800

    Deleted plot_splom

--- tests/testthat/test-plot_corr.R
+++ tests/testthat/test-plot_corr.R
@@ -13,7 +13,7 @@ test_that('FAILED',{
   # # Check if results are correct when no optional hyper-parameters are provided)
   results <- plot_corr(df)
 
-  test_that('Plot should use geom_bar and map x to x-axis', {
+  test_that('Plot should use Ggally corr and map x to x-axis', {
     expect_true( "GeomRect" %in% class(results$layers[[1]]$geom))
     expect_true("x" == rlang::get_expr(results$mapping$x))
     expect_true("y" == rlang::get_expr(results$mapping$y))

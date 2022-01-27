b98a529346649120ab9736ae7861f46253bd2f77
Sanchit Singh <sanchit120496@gmail.com>
2022-01-26 20:17:36 -0800

    Added classifier function and tests

--- R/fit_classifier.R
+++ R/fit_classifier.R
@@ -1,19 +1,95 @@
-#' Fit dummy classifier and Logistic regression models 
+#' Fit dummy classifier and Logistic regression models
 #'
 #' @param train_df dataframe that will be used to train the model
 #' @param target_col The column that needs to be classified as a string
 #' @param numeric_feats The numeric columns as a list
-#' @param categorical_features The categorical columns as a list
-#' @param cv The number of crossvalidation folds as an integer
+#' @param categorical_feats The categorical columns as a list
+#' @param cv The number of cross validation folds as an integer
 #'
 #' @return A data frame
 #' @export
 #'
 #' @examples
-#' fit_classifier(data, target_col = 'income', categorical_features=list('occupation'))
-#' fit_classifier(data, target_col = 'income', numeric_feats = list('age', 'fnlwgt', 'hours.per.week', 'education.num', 'capital.gain', 'capital.loss'), categorical_feats=list('genre'), cv=10)
-#' 
-#' df <- data.frame(x = c(1, 2, 4, 6, 8),y = c(0, 1, 1, 0, 1))
-#' fit_classifier(df, target_col = 'y', numeric_feats=list('x'))
-#' fit_classifier(df, target_col = 'y')
-fit_classifier <- function(train_df, target_col, numeric_feats= NULL, categorical_features= NULL, cv = 5){}
+#' library(tidyverse)
+#' library(caret)
+#' library(mltools)
+#' library(data.table)
+#' data <- data.frame(x = c(1, 2, 4, 6, 8), y = c("Yes", "No", "Maybe", "Someday", "Never"), z = c(0, 1, 1, 0, 1))
+#' fit_classifier(data, target_col = 'z', numeric_feats = list(), categorical_feats = list('y'), cv = 10)
+#' fit_classifier(data, target_col = 'z', numeric_feats = list('x'), categorical_feats = list('y'), cv = 5)
+
+fit_classifier <- function(train_df, target_col, numeric_feats= list(), categorical_feats= list(), cv = 5){
+    
+    library(dplyr)
+    #install.packages("caret")
+    library(caret)
+    #install.packages("mltools")
+    #install.packages("data.table")
+    library(mltools)
+    library(data.table)
+    
+    if (!is.data.frame(train_df)){
+        stop('Please input a dataframe')
+    }
+    
+    if (is.null(target_col)){
+        stop('Please enter a target column')
+    }
+    
+    if (!is.list(numeric_feats)){
+        stop('Please enter a list for numeric columns')
+    }
+    
+    if (!is.list(categorical_feats)){
+        stop('Please enter a list for categorical columns')
+    }
+    
+    X_train <- train_df |> select(-target_col)
+    y_train <- train_df |> select(target_col)
+    
+    # If numeric features is passed as an empty list
+    if(length(numeric_feats) == 0 ) {
+        numeric_feats <- as.list(strsplit(c(colnames(select_if(X_train, is.numeric))), " "))
+    } else {
+        numeric_feats <- numeric_feats
+    }
+    
+    
+    # If categorical features is passed as an empty list
+    if(length(categorical_feats) == 0 ) {
+        categorical_feats <- as.list(strsplit(c(colnames(select_if(X_train, is.character))), " "))
+    } else {
+        categorical_feats <- categorical_feats
+    }
+    
+    
+    # Scaling numeric columns
+    X_train_numeric <- scale(X_train[, unlist(numeric_feats)])
+    
+    # One hot coding of categorical columns
+    X_train[, unlist(categorical_feats)] <- as.factor(X_train[, unlist(categorical_feats)])
+    X_train_categorical <- one_hot(as.data.table(X_train[, unlist(categorical_feats)]))
+    
+    # Combining pre-processed data
+    train_preprocessed <- cbind(X_train_numeric, X_train_categorical, y_train)
+    
+    # Model
+    set.seed(123) 
+    lr_model <- train(as.factor(income) ~., 
+                      data = train_preprocessed, 
+                      method = "glm", 
+                      family = "binomial", 
+                      trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
+    
+    # Dummy Classifier
+    lr_model_2 <- train(as.factor(income) ~., 
+                        data = train_preprocessed, 
+                        method = "null", 
+                        trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
+    
+    # Model Accuracy
+    
+    lr_results <- data.frame(dummy_classifier = lr_model_2$results[[2]], logistic_regression = lr_model$results[[2]])
+    
+    return(lr_results)
+}
--- tests/testthat/test-fit_classifier.R
+++ tests/testthat/test-fit_classifier.R
@@ -1,3 +1,33 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(dplyr)
+library(caret)
+library(mltools)
+library(data.table)
+
+test_that('FAILED',{
+    
+    data <- read_csv('../data/adult.csv')
+    data <- na.omit(data)
+    
+    # Splitting data into test and train
+    set.seed(123)
+    index <- sample(1:nrow(data), 0.8*nrow(data))
+    train_df <- data[index,] # Create the training data
+    test_df <- data[-index,] # Create the test data
+    
+    
+    expect_error(fit_classifier(1))
+    expect_error(fit_classifier(train_df, numeric_feats = list("age"), categorical_feats = list(), cv = 5))
+    expect_error(fit_classifier(train_df, target_col = "income", categorical_feats = list("occupation"), cv = 5))
+    expect_error(fit_classifier(train_df, target_col = "income", numeric_feats = list("age"), cv = 5))
+    
+    
+    test_that('Plot should use Ggally corr and map x to x-axis', {
+        expect_true(is.data.frame(fit_classifier(train_df,
+                                                 target_col = 'income',
+                                                 numeric_feats = list('age', 'fnlwgt', 'hours.per.week', 'education.num', 'capital.gain', 'capital.loss'),
+                                                 categorical_feats = list('occupation'),
+                                                 cv=5)))
+        
+    })
+    
 })

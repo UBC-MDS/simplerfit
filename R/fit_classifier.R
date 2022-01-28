b05c0a9757818dff82fa60658aaac8c61491b071
Sanchit Singh <sanchit120496@gmail.com>
2022-01-28 00:59:57 -0800

    Adding changes in the function and test file

--- R/fit_classifier.R
+++ R/fit_classifier.R
@@ -3,7 +3,7 @@
 #' @param train_df dataframe that will be used to train the model
 #' @param target_col The column that needs to be classified as a string
 #' @param numeric_feats The numeric columns as a list
-#' @param categorical_features The categorical columns as a list
+#' @param categorical_feats The categorical columns as a list
 #' @param cv The number of cross validation folds as an integer
 #'
 #' @return A data frame
@@ -11,8 +11,98 @@
 #'
 #' @examples
 #' library(tidyverse)
-#' fit_classifier(data, target_col = 'income', categorical_feats=c('occupation'))
-#' fit_classifier(data, target_col = 'income', numeric_feats = c('age', 'fnlwgt'), categorical_feats=c('genre'), cv=10)
-#'
+#' library(lattice)
+#' library(caret)
+#' library(mltools)
+#' library(data.table)
+#' library(gapminder)
+#' gapminder <- gapminder |> dplyr::filter(continent=="Asia" | continent=="Europe")
+#' gapminder$country <- as.character(gapminder$country)
+#' gapminder$continent <- as.character(gapminder$continent)
+#' gapminder$year <- as.numeric(gapminder$year)
+#' gapminder$pop <- as.numeric(gapminder$pop)
+#' fit_classifier(gapminder, target_col = 'continent', numeric_feats = list('gdpPercap'), categorical_feats = list('country'), cv = 5)
 
-fit_classifier <- function(train_df, target_col, numeric_feats= NULL, categorical_feats= NULL, cv = 5){}
+fit_classifier <- function(train_df, target_col, numeric_feats= NULL, categorical_feats= NULL, cv = 5){
+    
+    #library(dplyr)
+    #library(lattice)
+    #install.packages("caret")
+    #library(caret)
+    #install.packages("mltools")
+    #install.packages("data.table")
+    #library(mltools)
+    #library(data.table)
+    
+    if (!is.data.frame(train_df)){
+        stop('Please input a dataframe')
+    }
+    
+    if (is.null(target_col)){
+        stop('Please enter a target column')
+    }
+    
+    if (!is.list(numeric_feats)){
+        stop('Please enter a list for numeric columns')
+    }
+    
+    if (!is.list(categorical_feats)){
+        stop('Please enter a list for categorical columns')
+    }
+    
+    X_train <- train_df |> dplyr::select(-target_col)
+    y_train <- train_df |> dplyr::select(target_col)
+    
+    # If numeric features is passed as an empty list
+    if(length(numeric_feats) == 0 ) {
+        numeric_feats <- as.list(strsplit(c(colnames(dplyr::select_if(X_train, is.numeric))), " "))
+    } else {
+        numeric_feats <- numeric_feats
+    }
+    
+    
+    # If categorical features is passed as an empty list
+    if(length(categorical_feats) == 0 ) {
+        categorical_feats <- as.list(strsplit(c(colnames(select_if(X_train, is.character))), " "))
+    } else {
+        categorical_feats <- categorical_feats
+    }
+    
+    
+    # Scaling numeric columns
+    X_train_numeric <- scale(X_train[, unlist(numeric_feats)])
+    
+    # One hot coding of categorical columns
+    X_train[unlist(categorical_feats)] <- lapply(X_train[unlist(categorical_feats)], factor)
+    X_train_categorical <- one_hot(as.data.table(X_train[, unlist(categorical_feats)]))
+    
+    # Combining pre-processed data
+    train_preprocessed <- cbind(X_train_numeric, X_train_categorical, y_train)
+    
+    # new column for the model
+    # train_preprocessed$target_col_model <- train_preprocessed |> dplyr::select(target_col)
+    # train_preprocessed <- train_preprocessed |> dplyr::select(-target_col)    # train_preprocessed$target_col_model <- train_preprocessed |> dplyr::select(target_col)
+    # train_preprocessed <- train_preprocessed |> dplyr::select(-target_col)
+    # train_preprocessed$target_col_model <- as.factor(train_preprocessed$target_col_model)
+    
+    # Model
+    set.seed(123) 
+    lr_model <- train(as.factor(continent) ~., 
+                      data = train_preprocessed, 
+                      method = "glm", 
+                      family = "binomial", 
+                      trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
+    
+    # Dummy Classifier
+    lr_model_2 <- train(as.factor(continent) ~., 
+                        data = train_preprocessed, 
+                        method = "null", 
+                        trControl = trainControl(method = "cv", number = cv, savePredictions=TRUE))
+    
+    # Model Accuracy
+    
+    lr_results <- data.frame(dummy_classifier = lr_model_2$results[[2]], logistic_regression = lr_model$results[[2]])
+    
+    return(lr_results)
+}
+
